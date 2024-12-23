06d876333abfad20e38016dbb1f987d98b616c9f
QA-K <jkwon@bioconnect.com>
2024-12-23 13:45:13 -0500

    [Update]sitemap_xml

--- Pages/Main/footer.php
+++ Pages/Main/footer.php
@@ -1,6 +1,18 @@
-<!-- Main/footer.php -->
 <footer>
-    <p>&copy; 2024 My Website. All rights reserved.</p>
+    <!-- Sitemap Link -->
+    <p style="font-size: 12px; text-align: center;">
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/sitemap.xml'); ?>" target="_blank">Sitemap Test</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Login/login.php'); ?>">Login</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Main/home.php'); ?>">Home</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Main/about.php'); ?>">About</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Main/projects.php'); ?>">Projects</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Main/contact.php'); ?>">Contact</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Teams/team.html'); ?>">Our Team</a> | 
+        <a href="<?php echo htmlspecialchars('https://QA-K.github.io/Teams/history.html'); ?>">Our History</a>
+    </p>
+
+    <!-- Copyright Information -->
+    <p style="font-size: 12px; text-align: center;">
+        &copy; 2024 My Website. All rights reserved.
+    </p>
 </footer>
-</body>
-</html>
